5a57fda8654a15bdc643a3060a3d3774f8b63aa1
Moe Abdalla <m.da7th@gmail.com>
2024-06-19 07:07:07 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,26 +1,26 @@
 // create web server
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-// get body-parser
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// // get body-parser
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
 
-// get comments
-const comments = require('./comments')
-app.get('/comments', (req, res) => {
-  res.json(comments.getComments());
-});
+// // get comments
+// const comments = require('./comments')
+// app.get('/comments', (req, res) => {
+//   res.json(comments.getComments());
+// });
 
-// post comments
-app.post('/comments', (req, res) => {
-  const {name, comment} = req.body;
-  comments.addComment(name, comment);
-  res.json(comments.getComments());
-});
+// // post comments
+// app.post('/comments', (req, res) => {
+//   const {name, comment} = req.body;
+//   comments.addComment(name, comment);
+//   res.json(comments.getComments());
+// });
 
-// listen port 3000
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+// // listen port 3000
+// app.listen(3000, () => {
+//   console.log('Server is running on http://localhost:3000');
+// });
